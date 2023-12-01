8dd94dcbd629adfaf319ee0649a872901368072d
AyanF <ayanfarooqui111@gmail.com>
2023-12-01 17:01:16 +0530

    Updated instance name, removed cloud watch, updated AMI

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "AMI" {
 description = "The AMI to use for the instance"
   type        = string
-  default     = "ami-032ee1d9ab851471f"
+  default     = "ami-00277a87b87c84d18"
 }
 variable "EC2_INSTANCE_TYPE" {
 description = "Value of the Instance configuration"
@@ -11,7 +11,7 @@ description = "Value of the Instance configuration"
 variable "az_zone" {
 description = "Availability Zone the instance is launched in"
   type        = string
-  default     = "us-east-1d"
+  default     = "us-west-2a"
 }
 variable "EC2_ROOT_VOLUME_SIZE" {
 description = "Value of the Instance storage size"
@@ -21,15 +21,7 @@ description = "Value of the Instance storage size"
 variable "INSTANCE_NAME" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "omsinstance"
+  default     = "moqui-instance"
 }
-#CloudWatch
-variable "alarm_name" {
-description = "The AMI to use for the instance"
-  type        = string
-  default     = "ec2-cpu-alarm"
-}
-variable "sns_topic_name" {
-  description = "CloudWatch SMS Topic Name"
-  default     = "arn:aws:sns:us-east-1:289432782788:Sandeep"
-}
+
+
